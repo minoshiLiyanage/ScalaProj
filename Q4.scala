651d1669aa10a25cfa5d341255b39667b8c1656d
Rishie Ramasinghe <35812345+rishier827@users.noreply.github.com>
2020-10-17 01:34:51 +0530

    Update Q4.scala

--- Q4.scala
+++ Q4.scala
@@ -1,11 +1,11 @@
 object Q4{
   
   def main(args: Array[String]){
-  	var m=26.0f;
-    var y=385.0f;
-    m = m + y/1760;
-    var km = m*1.609;
-    var m = km*1000;
+  	var m=260.0f;
+    var y=3850.0f;
+    m = m + y/17600;
+    var km = m*1.6009;
+    var m = km*10000;
     
     println(km);
   }
